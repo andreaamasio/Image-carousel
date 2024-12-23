c696f2404b084676ec4f9e7f48ce7abf5becfa01
andreaamasio <101635283+andreaamasio@users.noreply.github.com>
2024-12-23 09:59:30 +0100

    fully working, some code may be repeated less for displayNextSlide

--- src/script.js
+++ src/script.js
@@ -18,31 +18,30 @@ const imageAltArray=[
 const slidesArray=[]
 
 for (let i = 0; i<imageSrcArray.length;i++){
-    let newImage = createSlide(
+    let newSlide = createSlide(
         imageSrcArray[i],
         imageAltArray[i],
         `slide-${i}`
     )
-    slidesArray.push(newImage)
+    slidesArray.push(newSlide)
 }
+
+const imageElement=document.querySelector("div.image-container img") 
 function getSlide(id){
     return slidesArray.find((slide)=>
         slide.id === id
     )
 }
-
-function displayImage(event){
-    const allLi=document.querySelectorAll("li")
+// logic for showing slide
+function displaySlide(slideSelected, slideID){
     allLi.forEach(li => {
         li.setAttribute("style","list-style:circle")
+        
     });
     
-    let slideID = event.currentTarget.id
-    console.log(slideID)
-    let slideSelected = getSlide(slideID)
-    console.log(slideSelected)
     imageElement.setAttribute("src", slideSelected.src)
     imageElement.setAttribute("alt", slideSelected.alt)
+    imageElement.setAttribute("class", slideID)
     liTarget=document.querySelector(`#${slideID}`)
     liTarget.setAttribute("style","list-style:disc")
 }
@@ -51,14 +50,55 @@ const dotContainer=document.querySelector("#dot-container")
 slidesArray.forEach(slide => {
     let newLi = document.createElement("li")
     newLi.setAttribute("id", slide.id)
-    newLi.addEventListener("click", displayImage)
+    newLi.addEventListener("click", collectID)
     dotContainer.appendChild(newLi)
 });
 
-const imageElement=document.querySelector("div.image-container img")
+const allLi=document.querySelectorAll("li")
+// dot event handler
+function collectID(event){
+    let slideID = event.currentTarget.id
+    let slideSelected = getSlide(slideID)
+    displaySlide(slideSelected, slideID)
+}
 
+//buttons left and right
+const buttonLeft = document.querySelector("button.left")
+const buttonRight = document.querySelector("button.right")
 
+buttonRight.addEventListener("click", displayNextOrPreviousSlide)
+buttonLeft.addEventListener("click", displayNextOrPreviousSlide)
 
+function displayNextOrPreviousSlide(event){
+    let currentSlideShownID=imageElement.classList[0]
+    let slideTargetID
+    if (event.currentTarget.classList[0]==="right"){
+        slideTargetID= parseInt(currentSlideShownID.split("-")[1]) + 1
+    } else slideTargetID= parseInt(currentSlideShownID.split("-")[1]) - 1
+    if (slideTargetID>slidesArray.length-1) {
+        slideTargetID = 0
+    }
+    if (slideTargetID<0) {
+        slideTargetID = slidesArray.length-1
+    }
+    let slideID = `slide-${slideTargetID}`
+    displaySlide(getSlide(slideID),slideID)
+}
+
+function displayNextSlide(){
+    currentSlideShownID=imageElement.classList[0]
+    let slideTargetID= parseInt(currentSlideShownID.split("-")[1]) + 1
+    if (slideTargetID>slidesArray.length-1) {
+        slideTargetID = 0
+    }
+    if (slideTargetID<0) {
+        slideTargetID = slidesArray.length-1
+    }
+    let slideID = `slide-${slideTargetID}`
+    displaySlide(getSlide(slideID),slideID)
+} 
+
+const intervalID = window.setInterval(displayNextSlide, 5000)
 
 
 
